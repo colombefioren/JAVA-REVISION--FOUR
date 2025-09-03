a3b0331279dae71a2f972ab2664bb24022b3915a
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-03 19:29:58 +0300

    feat: PriceRecord class to track dish price history

--- src/main/java/revision/exo3/Dish.java
+++ src/main/java/revision/exo3/Dish.java
@@ -1,8 +1,11 @@
 package revision.exo3;
 
+import java.util.List;
+
 public class Dish {
   private final int id;
   private final String name;
+  private List<PriceRecord> priceRecordList;
 
   public Dish(int id, String name) {
     this.id = id;
---
+++ src/main/java/revision/exo3/PriceRecord.java
@@ -0,0 +1,45 @@
+package revision.exo3;
+
+import java.time.Instant;
+
+public class PriceRecord {
+    private final int id;
+    private Instant date;
+    private double price;
+    private String reason;
+
+    public PriceRecord(int id, Instant date, double price, String reason) {
+        this.id = id;
+        this.date = date;
+        this.price = price;
+        this.reason = reason;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
