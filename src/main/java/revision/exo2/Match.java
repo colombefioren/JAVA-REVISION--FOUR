dcd9f0948bfc0c6d021dce20fc0df1b6407441e6
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-03 18:08:41 +0300

    feat: the two fighters in a match

--- src/main/java/revision/exo2/Match.java
+++ src/main/java/revision/exo2/Match.java
@@ -6,11 +6,23 @@ public class Match {
   private final int id;
   private final LocalDate date;
   private final String location;
+  private final Fighter fighter1;
+  private final Fighter fighter2;
 
-  public Match(int id, LocalDate date, String location) {
+  public Match(int id, LocalDate date, String location, Fighter fighter1, Fighter fighter2) {
     this.id = id;
     this.date = date;
     this.location = location;
+    this.fighter1 = fighter1;
+    this.fighter2 = fighter2;
+  }
+
+  public Fighter getFighter1() {
+    return fighter1;
+  }
+
+  public Fighter getFighter2() {
+    return fighter2;
   }
 
   public int getId() {
